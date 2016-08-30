ed5600a765fa34f7f6eaa381eaa5e0059b245a5e
Luke Brady <luke@isajediknight.com>
2016-08-29 20:10:33 -0700

    updating master to practice getting changes from other branches

--- fun.py
+++ fun.py
@@ -1,3 +1,4 @@
 print("This is my first branch!")
 answer = raw_input("Do you like it?")
-print("Feature_1_b is adding code here")
+print("Feature_1_b is adding code here"
+print("master branch changes"))
